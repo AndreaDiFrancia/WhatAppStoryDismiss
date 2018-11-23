64a415c22da95e903e8541e16d898cca8d5ed728
WhiteTiger “sgama la rete” <angelwhite@users.sf.net>
2018-11-23 15:54:31 +0100

    refactoring
    ;-P

--- EffectStoryWA/EffectStoryWA/Page2ViewController.swift
+++ EffectStoryWA/EffectStoryWA/Page2ViewController.swift
@@ -5,69 +5,77 @@
 import UIKit
 
 class Page2ViewController: UIViewController {
-
-    @IBOutlet weak var image: UIImageView?
-   
-    weak var mask: CAShapeLayer?
-    var initialCenter = CGPoint()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let mask = CAShapeLayer()
-        mask.fillColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-        mask.strokeColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).cgColor
-        image?.layer.mask = mask
-        self.mask = mask
+    @IBOutlet weak var image: UIImageView? {
+        willSet {
+            let mask = CAShapeLayer()
+            
+            newValue?.layer.mask = mask
+        }
     }
+    
+    private var initialCenter = CGPoint.zero
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         // update the mask for the image view for the size of the view (which was unknown at `viewDidLoad`, but is known here)
-        
-        if mask != nil {
-            updatePath(percent: 0)
-        }
+        updatePath(percent: 0.0)
     }
-
+    
+    deinit {
+        print("deinit")
+    }
+    
     @IBAction func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
-        guard gestureRecognizer.view != nil else {return}
-        let piece = gestureRecognizer.view!
-        // Get the changes in the X and Y directions relative to
-        // the superview's coordinate space.
-        var translation = gestureRecognizer.translation(in: piece.superview)
+        guard let gestureView = gestureRecognizer.view else {
+            return
+        }
         
-        if gestureRecognizer.state == .began {
+        let state = gestureRecognizer.state
+        
+        if state == .began {
             // Save the view's original position.
+            initialCenter = gestureView.center
             
-             self.initialCenter = piece.center
-        }
-        
-        // Update the position for the .began, .changed, and .ended states
-        if gestureRecognizer.state == .changed {
+        } else if state == .changed {
+            // Get the changes in the X and Y directions relative to
+            // the superview's coordinate space.
+            // Update the position for the .began, .changed, and .ended states
+            let newCenterY = gestureRecognizer.translation(in: gestureView.superview).y + initialCenter.y
             
-            translation = gestureRecognizer.translation(in: piece.superview)
+            updatePath(percent: newCenterY)
             
-            let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
-            
-            updatePath(percent: newCenter.y)
-            print(newCenter.y)
-        }
-        
-        if gestureRecognizer.state == .ended {
+        } else if gestureRecognizer.state == .ended {
             dismiss(animated: true) {
-                self.image?.layer.mask = nil
+                // NOP
             }
         }
     }
-
+    
     private func updatePath(percent: CGFloat) {
-        let center = CGPoint(x: (image?.bounds.midX)!, y: image!.bounds.midY)
-        let endRadius = min((image?.bounds.width)!, (image?.bounds.height)!) * 0.4  // stop radius is 40% of smallest dimension
-        let startRadius = hypot((image?.bounds.width)!, (image?.bounds.height)!) * 0.6  // start radius is the distance from the center to the corner of the image view
-        let radius = startRadius + (endRadius - startRadius) * percent/1000                // given percent done, what is the radius
+        guard let image = image else {
+            return
+        }
         
-        mask?.path = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: .pi * 2, clockwise: true).cgPath
+        let center = image.center
+        
+        // stop radius is 40% of smallest dimension
+        let endRadius = min(image.bounds.width, image.bounds.height) * 0.4
+        
+        // start radius is the distance from the center to the corner of the image view
+        let startRadius = hypot(image.bounds.width, image.bounds.height) * 0.6
+        
+        // given percent done, what is the radius
+        let radius = startRadius + (endRadius - startRadius) * (percent / 1000)
+        
+        let mask = image.layer.mask as? CAShapeLayer
+        
+        mask?.path = UIBezierPath(
+            arcCenter: center,
+            radius: radius,
+            startAngle: 0.0,
+            endAngle: .pi * 2,
+            clockwise: true
+            ).cgPath
     }
 }
